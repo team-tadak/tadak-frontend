c1e43cedb8a6c2cb73e4a28e14c59dec51820355
장재원 <jaychang99@scg.skku.ac.kr>
2022-11-03 01:51:05 +0900

    feat(frontend): TypeInput 컴포넌트의 Container 와 ContentBox 제작 완료

---
+++ src/components/TypeInput/index.js
@@ -0,0 +1,12 @@
+import { ContentBox, TypeInputContainer } from "components/TypeInput/styles";
+import React from "react";
+
+function TypeInput() {
+  return (
+    <TypeInputContainer>
+      <ContentBox></ContentBox>
+    </TypeInputContainer>
+  );
+}
+
+export default TypeInput;
---
+++ src/components/TypeInput/styles.js
@@ -0,0 +1,16 @@
+import styled from "@emotion/styled";
+
+export const TypeInputContainer = styled.div`
+  width: 770px;
+  background-color: white;
+  border-radius: 10px;
+  padding: 35px;
+`;
+
+export const ContentBox = styled.article`
+  border: 1px solid #bfbfbf;
+  border-radius: 10px;
+  padding: 13px 20px 0;
+`;
+
+export const StyledInput = styled.div``;
