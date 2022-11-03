c8d8563faf71defccaa73863137c44f8d5b52073
장재원 <jaychang99@scg.skku.ac.kr>
2022-11-03 14:46:03 +0900

    refactor(frontend): generateLetterStatus 리팩토링

--- src/components/TypeInput/index.js
+++ src/components/TypeInput/index.js
@@ -10,10 +10,20 @@ import { Paragraph } from "./styles";
 const MOCKUP_STRING =
   "Authors often misinterpret the lettuce as a folklore rabbi, when in actuality it feels more like an uncursed bacon. Pursued d";
 
+function generateLetterStatus(currentInputString, letter, currentLetterIndex) {
+  return currentInputString.length - 1 < currentLetterIndex
+    ? "untyped" // 아직 입력하지 않은 글자
+    : currentInputString[currentLetterIndex] === letter
+    ? "correct" // 입력을 했고, 맞는 글자
+    : "wrong"; // 입력은 했으나, 틀린 글자
+}
+
 function TypeInput() {
   const [currentInputString, setCurrentInputString] = useState("");
   const hiddenInputRef = useRef();
 
+  // 글자 부분을 클릭하면 자동으로 hidden Input 을 focusing 하게 하고,
+  // state 도 업데이트 하게 변경
   function handleContentBoxClick() {
     hiddenInputRef.current.focus();
     setCurrentInputString(hiddenInputRef.current.value);
@@ -26,13 +36,7 @@ function TypeInput() {
         <Paragraph>
           {MOCKUP_STRING.split("").map((letter, index) => (
             <Letter
-              status={
-                currentInputString.length - 1 < index
-                  ? "untyped"
-                  : currentInputString[index] === letter
-                  ? "correct"
-                  : "wrong"
-              }
+              status={generateLetterStatus(currentInputString, letter, index)}
               key={index}
             >
               {letter}
