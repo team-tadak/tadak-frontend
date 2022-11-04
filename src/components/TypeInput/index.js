1961b62c132a13a4bfc537ace166f6ec01fafb23
장재원 <jaychang99@scg.skku.ac.kr>
2022-11-05 01:27:08 +0900

    refactor(frontend): 기존 hiddenInput  방식의 입력에서 keyboardEvent 관찰 후 state 업데이트 방식으로 입력 방식 변경

--- src/components/TypeInput/index.js
+++ src/components/TypeInput/index.js
@@ -5,7 +5,7 @@ import {
   TypeInputContainer,
   HiddenInput,
 } from "components/TypeInput/styles";
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useCallback, useEffect } from "react";
 import { Paragraph } from "./styles";
 
 const MOCKUP_STRING =
@@ -31,8 +31,32 @@ function TypeInput() {
   // state 도 업데이트 하게 변경
   function handleContentBoxClick() {
     hiddenInputRef.current.focus();
-    setCurrentInputString(hiddenInputRef.current.value);
+    // setCurrentInputString(hiddenInputRef.current.value);
   }
+
+  const keyPress = useCallback(
+    (e) => {
+      // if Backspace < 이거 windows 대응 이슈 있을수도??
+      if (e.keyCode === 8) {
+        setCurrentInputString(currentInputString.slice(0, -1));
+      }
+      // 나머지 모든 키들에 대해서는
+      else {
+        // 눌러진 키가 한 글자인지 점검하는 건 Control, Alt, Shift, Backsapce 등
+        // 이런 실제 값이 아닌 조작용 키들이 바로 input 으로 먹어버리는 걸 검증하기 위함.
+        if (e.key.length === 1) {
+          setCurrentInputString(currentInputString + e.key);
+        }
+        // console.log(e.key, "||", e.keyCode);
+      }
+    },
+    [currentInputString]
+  );
+
+  useEffect(() => {
+    document.addEventListener("keydown", keyPress);
+    return () => document.removeEventListener("keydown", keyPress);
+  }, [keyPress]);
 
   return (
     <TypeInputContainer>
