ef994518a894579cb66d02970e262ced58ad36e5
jaychang99 <jaychang99@gmail.com>
2022-11-05 14:53:13 +0900

    feat(frontend): Emotion 전역 theme-sheet 세팅 및 ThemeProvider 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { ThemeProvider } from "@emotion/react";
 import FitContent from "components/common/layout/FitContent";
 import TypeInput from "components/TypeInput";
+import { theme } from "styles/theme";
 
 function App() {
   return (
-    <FitContent>
-      <TypeInput />
-    </FitContent>
+    <ThemeProvider theme={theme}>
+      <FitContent>
+        <TypeInput />
+      </FitContent>
+    </ThemeProvider>
   );
 }
 
---
+++ src/styles/theme.js
@@ -0,0 +1,7 @@
+export const theme = {
+  color: {
+    blue: {
+      light: "#74ADE9",
+    },
+  },
+};
