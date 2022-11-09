64b77a10a96d6580eaa1c1b7122e9d9b045c5879
skkuchoi <cyj2017@naver.com>
2022-11-09 14:00:56 +0900

    complete router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+//import styled from "@emotion/styled";
+
+import LanguageSelect from "pages/LanguageSelect";
+import Leaderboard from "pages/Leaderboard";
+import Login from "pages/Login";
+import Main from "pages/Main";
+import MyPage from "pages/MyPage";
+import Register from "pages/Register";
+import SyntaxSelect from "pages/SyntaxSelect";
+import TypePage from "pages/TypePage";
+
 import { ThemeProvider } from "@emotion/react";
+//import FitContent
 import { theme } from "styles/theme";
-import GlobalStyles from "styles/GlobalStyles";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyles />
+      <Router>
+        <Routes>
+          <Route path="/languageselect" exact={true} element={<LanguageSelect />} />
+          <Route path="/leaderboard" exact={true} element={<Leaderboard />} />
+          <Route path="/login" exact={true} element={<Login />} />
+          <Route path="/" exact={true} element={<Main />} />
+          <Route path="/mypage" exact={true} element={<MyPage />} />
+          <Route path="/register" exact={true} element={<Register />} />
+          <Route path="/syntaxselect" exact={true} element={<SyntaxSelect />} />
+          <Route path="/" exact={true} element={<TypePage />} />
+        </Routes>
+      </Router>
     </ThemeProvider>
   );
 }
