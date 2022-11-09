d8fedda1db756f8ff1e5e45af9599180e290725d
skkuchoi <cyj2017@naver.com>
2022-11-09 22:22:08 +0900

    Complete of Router

--- src/App.js
+++ src/App.js
@@ -20,14 +20,29 @@ function App() {
     <ThemeProvider theme={theme}>
       <Router>
         <Routes>
+          {/* 언어 선택 페이지 */}
           <Route path="/languageselect" exact={true} element={<LanguageSelect />} />
+
+          {/* 랭킹 체이지 */}
           <Route path="/leaderboard" exact={true} element={<Leaderboard />} />
+
+          {/* 로그인 페이지 */}
           <Route path="/login" exact={true} element={<Login />} />
+
+          {/* 대문 페이지 */}
           <Route path="/" exact={true} element={<Main />} />
+
+          {/* 마이 페이지 */}
           <Route path="/mypage" exact={true} element={<MyPage />} />
+
+          {/* 회원가입 페이지 */}
           <Route path="/register" exact={true} element={<Register />} />
+
+          {/* 문법 선택 페이지 */}
           <Route path="/syntaxselect" exact={true} element={<SyntaxSelect />} />
-          <Route path="/" exact={true} element={<TypePage />} />
+
+          {/* 타자 연습 페이지 */}
+          <Route path="/typepage" exact={true} element={<TypePage />} />
         </Routes>
       </Router>
     </ThemeProvider>
