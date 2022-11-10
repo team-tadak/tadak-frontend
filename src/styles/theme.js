0c0329341cefd5ace5ca245d8aee6f7d535c212c
jaychang99 <jaychang99@gmail.com>
2022-11-10 14:54:42 +0900

    feat: theme.ts 에 primary 500 색상 추가

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -8,6 +8,7 @@ export const theme = {
     },
     gray: "#5D636F",
     white: "#FFFFFF",
+    primary500: "#4760FA",
     warning300: "#F3B161",
     background: "#101217",
     navbarBG: "#202737",
