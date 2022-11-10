e1395d676f355bf93038a867367ed212848d1091
jaychang99 <jaychang99@gmail.com>
2022-11-10 23:48:24 +0900

    feat: my page 레이아웃 및 기본 UI 구현 (navigation 및 API 연결 필요

--- src/pages/MyPage/index.js
+++ src/pages/MyPage/index.js
@@ -2,11 +2,13 @@ import { MyPageContainer } from "pages/MyPage/styles";
 import React from "react";
 import { staggerQuarter, defaultFadeInVariants } from "styles/motions";
 import Typography from "components/common/Typography";
+import HistoryBoard from "components/HistoryBoard";
 
 function MyPage() {
   return (
     <MyPageContainer variants={staggerQuarter} initial="initial" whileInView="animate" exit="exit">
       <Typography variants={defaultFadeInVariants}>내 플레이 기록</Typography>
+      <HistoryBoard />
     </MyPageContainer>
   );
 }
