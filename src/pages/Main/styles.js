78de8045a9cf9f308658caab8238a78ae86aa113
jaychang99 <jaychang99@gmail.com>
2022-11-10 13:58:24 +0900

    feat: Main 화면 Input 부분 제외 전부 구성

---
+++ src/pages/Main/styles.js
@@ -0,0 +1,46 @@
+import styled from "@emotion/styled/macro";
+import { motion } from "framer-motion";
+
+export const MainPageContainer = styled.main`
+  display: flex;
+  min-height: 100vh;
+`;
+
+export const MainPageContentSection = styled.section`
+  flex-shrink: 0;
+`;
+export const MainPageImageSection = styled.section`
+  flex-grow: 1;
+`;
+
+export const Title = styled(motion.h1)`
+  margin-top: 160px;
+
+  font-size: 70px;
+  font-weight: 700;
+  color: ${({ theme }) => theme.color.white};
+`;
+export const SubTitle = styled(motion.p)`
+  margin-top: 8px;
+
+  font-size: 28px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.color.white};
+
+  &:first-of-type {
+    margin-top: 16px;
+  }
+`;
+
+export const MainPageImage = styled(motion.div)`
+  background-image: url("images/main_image.png");
+
+  background-size: cover;
+  background-repeat: no-repeat;
+
+  /* 블러 효과  */
+  filter: blur(4px);
+  -webkit-filter: blur(4px);
+
+  height: 1000px;
+`;
