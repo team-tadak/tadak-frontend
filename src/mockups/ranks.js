16ac41647ec0022a33305ded50c0e1c57c4f24d9
jaychang99 <jaychang99@gmail.com>
2022-11-10 16:29:14 +0900

    feat: ranks 관련 mockup data 추가

---
+++ src/mockups/ranks.js
@@ -0,0 +1,37 @@
+export const MOCKUP_RANKS = {
+  ranks: [
+    {
+      user: {
+        id: 1,
+        username: "태정태세문단세",
+        image_no: 1,
+      },
+      language_no: 1,
+      grammar_no: 2,
+      record: 1022,
+      createdAt: "2022-10-27T12:34:33",
+    },
+    {
+      user: {
+        id: 2,
+        username: "고독한미식가",
+        image_no: 2,
+      },
+      language_no: 1,
+      grammar_no: 6,
+      record: 1022,
+      createdAt: "2022-10-27T12:34:33",
+    },
+    {
+      user: {
+        id: 3,
+        username: "씸던껍",
+        image_no: 3,
+      },
+      language_no: 1,
+      grammar_no: 3,
+      record: 1022,
+      createdAt: "2022-10-27T12:34:33",
+    },
+  ],
+};
