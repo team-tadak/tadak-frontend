88a1cf5a3ca9566d0778fea81c99ea1a30e188bd
jaychang99 <jaychang99@gmail.com>
2022-11-10 16:30:24 +0900

    feat: Badge 컴포넌트 추가 (랭킹 및 사이드바 등에 이용되는 아이콘이 있는 텍스트)

---
+++ src/components/common/Badge/styles.js
@@ -0,0 +1,23 @@
+import styled from "@emotion/styled/macro";
+
+export const StyledBadge = styled.div`
+  display: flex;
+  column-gap: 6px;
+  align-items: center;
+`;
+
+export const BadgeIconContainer = styled.div`
+  width: 24px;
+  height: 24px;
+
+  /* 아이콘 중앙 정렬 */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const BadgeText = styled.div`
+  font-weight: 400;
+  font-size: 14px;
+  color: ${(props) => props.theme.color.white};
+`;
