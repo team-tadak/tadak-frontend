9c903d0a25da1394c88482484b43a8cff09730e5
tony <yontags@gmail.com>
2022-11-11 01:59:49 +0900

    breadcrumb complete..?

---
+++ src/components/common/Breadcrumb/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { StyledBreadcrumb, BreadcrumbColor, BreadcrumbShown } from "components/common/Badge/styles";
+import { Link, useLocation } from "react-router-dom";
+
+function Breadcrumbs() {
+  const location = useLocation();
+  return (
+    <StyledBreadcrumb>
+      <BreadcrumbShown status={location.pathname.startsWith("/") ? "shown" : "notShown"}>
+        <Link to="/">
+          <BreadcrumbColor
+            status={
+              !location.pathname.startsWith("/languageselect")
+                ? "breadcrumb-active"
+                : "breadcrumb-not-active"
+            }
+          >
+            ttdtt
+          </BreadcrumbColor>
+        </Link>
+      </BreadcrumbShown>
+      <BreadcrumbShown status={location.pathname.startsWith("/python") ? "shown" : "notShown"}>
+        <Link to="/python">
+          <span className="breadcrumb-arrow">&gt;</span>
+          <BreadcrumbColor
+            status={
+              !location.pathname.startsWith("/python/typePage")
+                ? "breadcrumb-active"
+                : "breadcrumb-not-active"
+            }
+          >
+            python
+          </BreadcrumbColor>
+        </Link>
+      </BreadcrumbShown>
+      <BreadcrumbShown
+        status={location.pathname.startsWith("/python/typePage") ? "shown" : "notShown"}
+      >
+        <Link>
+          <span className="breadcrumb-arrow">&gt;</span>
+          <BreadcrumbColor status="breadcrumb-active">while.html</BreadcrumbColor>
+        </Link>
+      </BreadcrumbShown>
+    </StyledBreadcrumb>
+  );
+}
+
+export default Breadcrumbs;
+
---
+++ src/components/common/Breadcrumb/style.js
@@ -0,0 +1,30 @@
+import styled from "@emotion/styled/macro";
+
+export const StyledBreadcrumb = styled.nav`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 16px;
+  gap: 10px;
+
+  width: 1280px;
+  height: 51px;
+
+  background: #161b21;
+  color: #FFFFFF
+  border-radius: 10px;
+`;
+
+export const BreadcrumbShown = styled.div`
+  visibility: ${(props) => (props.status === "shown" ? "visible" : "hidden")};
+`;
+
+export const BreadcrumbColor = styled.div`
+  color: ${(props) =>
+    props.status === "breadcrumb-not-active"
+      ? props.theme.color.white
+      : props.theme.color.primary500};
+  &:hover {
+    text-decoration: underline;
+  }
+`;
