17843e7dd213835e37760782cbcb6aadb9728fc6
jaychang99 <jaychang99@gmail.com>
2022-11-10 10:47:05 +0900

    feat: NavBar 에서 link 및 로그인 여부별 분기 작업 추가

--- src/components/common/NavBar/index.js
+++ src/components/common/NavBar/index.js
@@ -11,30 +11,49 @@ import {
   NavBarMenuText,
   StyledNavBar,
 } from "components/common/NavBar/styles";
+import useUser from "hooks/useUser";
+import { MOCKUP_USER } from "mockups/users";
 import React from "react";
 import { Link } from "react-router-dom";
 
 function NavBar() {
+  const { loggedOut } = useUser();
+
   return (
     <StyledNavBar>
       <NavBarContainer>
-        <NavBarLogoContainer>
-          <Logo />
-          <NavBarLogoText>TADAK</NavBarLogoText>
-        </NavBarLogoContainer>
+        <Link to={"/"}>
+          <NavBarLogoContainer>
+            <Logo />
+            <NavBarLogoText>TADAK</NavBarLogoText>
+          </NavBarLogoContainer>
+        </Link>
         <NavBarMenuContainer>
-          <NavBarMenuText>
-            <Link to={"/login"}>로그인</Link>
-          </NavBarMenuText>
-          <NavBarMenuText>
-            <Link to={"/register"}>회원가입</Link>
-          </NavBarMenuText>
-          <NavBarLogoutContainer>
-            <NavBarLogoutText>로그아웃</NavBarLogoutText>
-            <NavBarLogoutIcon>
-              <LogoutIcon />
-            </NavBarLogoutIcon>
-          </NavBarLogoutContainer>
+          {loggedOut ? (
+            <>
+              <NavBarMenuText>
+                <Link to={"/login"}>로그인</Link>
+              </NavBarMenuText>
+              <NavBarMenuText>
+                <Link to={"/register"}>회원가입</Link>
+              </NavBarMenuText>
+            </>
+          ) : (
+            <>
+              <NavBarMenuText>{MOCKUP_USER.username}님 환영합니다!</NavBarMenuText>
+              <NavBarMenuText>
+                <Link to={"/mypage"}>마이페이지</Link>
+              </NavBarMenuText>
+              <NavBarLogoutContainer>
+                <NavBarLogoutText>
+                  <Link to={"/"}>로그아웃</Link>
+                </NavBarLogoutText>
+                <NavBarLogoutIcon>
+                  <LogoutIcon />
+                </NavBarLogoutIcon>
+              </NavBarLogoutContainer>
+            </>
+          )}
         </NavBarMenuContainer>
       </NavBarContainer>
     </StyledNavBar>
