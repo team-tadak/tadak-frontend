681ed1e360789ab910154d1a566f87d1f07f4339
jaychang99 <jaychang99@gmail.com>
2022-11-11 20:21:41 +0900

    chore: SyntaxSelect 페이지 컴포넌트 언어별 라우팅 및 예시 텍스트 구성

--- src/pages/SyntaxSelect/index.js
+++ src/pages/SyntaxSelect/index.js
@@ -1,25 +1,10 @@
-import SyntaxSelectComponent from "components/Syntax";
-import DropdownComponent from "components/Dropdown";
 import React from "react";
 
-function SyntaxSelect() {
+function SyntaxSelect({ language }) {
   return (
     <div>
-      {/* <SyntaxSelectComponent
-        example={
-          <p>
-            def sum(a, b):
-            <br />
-            &nbsp;&nbsp;&nbsp;&nbsp;result = a + b<br />
-            &nbsp;&nbsp;&nbsp;&nbsp;return result
-          </p>
-        }
-        title="함수 정의 및 사용"
-        level="2"
-        summary="파이썬에서 재사용을 위한 함수를 배웁니다! 함수는 프로그래밍 언어에서 핵심적입니다!"
-      />
-      <DropdownComponent list={["남성", "여성", "선택 안 함"]}></DropdownComponent> */}
-      SyntaxSelect
+      <p>{language} 언어를 선택해서 들어온 페이지입니다. </p>
+      <p>{language} 에서 어떤 문법으로 연습할지 선택을 할 수 있어야 합니다. </p>
     </div>
   );
 }
