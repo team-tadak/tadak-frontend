7bdad1a721fe81514875853c929aeff5718db833
jaychang99 <jaychang99@gmail.com>
2022-11-11 20:24:05 +0900

    chore: SyntaxSelectComponent  에 사용 예시 추가

--- src/components/Syntax/index.js
+++ src/components/Syntax/index.js
@@ -73,6 +73,22 @@ export const SyntaxStartButton = styled.button`
   font-weight: bold;
 `;
 
+// 사용 예시
+//
+// /<SyntaxSelectComponent
+// example={
+//   <p>
+//     def sum(a, b):
+//     <br />
+//     &nbsp;&nbsp;&nbsp;&nbsp;result = a + b<br />
+//     &nbsp;&nbsp;&nbsp;&nbsp;return result
+//   </p>
+// }
+// title="함수 정의 및 사용"
+// level="2"
+// summary="파이썬에서 재사용을 위한 함수를 배웁니다! 함수는 프로그래밍 언어에서 핵심적입니다!"
+// />
+
 function SyntaxSelectComponent({ example, title, level, summary }) {
   var stars = [];
   for (var i = 0; i < 5; i++) {
