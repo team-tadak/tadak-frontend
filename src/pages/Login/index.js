7b0fa49067252d6d4745a6b2148c952ecc078784
jaychang99 <jaychang99@gmail.com>
2022-11-11 20:38:44 +0900

    chore: 로그인 페이지 셋업

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -34,7 +34,24 @@ function Login() {
     login();
   }, []);
 
-  return <div>Login</div>;
+  return (
+    <div>
+      <p> 로그인 페이지입니다! </p>
+      <p>이 페이지를 수정하시려면 src/pages/Login/index.js 의 Login( ) 컴포넌트를 수정해주세요!</p>
+      <p>이 페이지에서는 사용자의 이메일과 비밀번호를 받아 로그인을 처리하는 페이지입니다.</p>
+      <p>사용하실 컴포넌트는 InputEmail, InputPasswordAndCheck 입니다.</p>
+      <p>
+        컴포넌트가 필요한 prop 을 받지 않고 있거나, 사소한 수정사항이 필요한 경우 직접 수정해주셔도
+        되고
+      </p>
+      <p>
+        **로그인 페이지는 API 연결을 필요로 하므로, 상단에 미리 API 통신 로직을 입력해두었습니다.
+      </p>
+      <p>**해당 부분은 컴포넌트 렌더링에는 전혀 영향이 없으므로 무시하시고 진행하시면 됩니다!</p>
+      <p>컴포넌트 제작자 또는, 저에게 문의하시면 됩니다!</p>
+      <p>그럼 화이팅입니다!</p>
+    </div>
+  );
 }
 
 export default Login;
