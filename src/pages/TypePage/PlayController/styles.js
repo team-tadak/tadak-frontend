1c703018f30e1ef17b4a78c5378f233cf1b03cbf
jaychang99 <jaychang99@gmail.com>
2022-11-11 15:48:39 +0900

    feat: 게임을 일시정지 및 재개할 수 있는 PlayController 컴포넌트 제작

---
+++ src/pages/TypePage/PlayController/styles.js
@@ -0,0 +1,31 @@
+import styled from "@emotion/styled/macro";
+import { motion } from "framer-motion";
+import { resetButtonStyle } from "styles/utils/button";
+
+export const StyledPlayController = styled(motion.div)`
+  display: flex;
+  justify-content: space-around;
+  column-gap: 20px;
+  width: 100%;
+`;
+
+export const PlayContollerButton = styled(motion.button)`
+  ${resetButtonStyle}
+  border-radius: 10px;
+
+  padding: 10px 0;
+  width: 100%;
+  font-size: 16px;
+  font-weight: 400;
+  color: ${({ theme, buttontype }) =>
+    buttontype === "warning" ? theme.color.warning300 : theme.color.white};
+
+  &:hover {
+    background-color: ${({ theme }) => theme.color.gray};
+  }
+  &:active {
+    background-color: ${({ theme }) => theme.color.grayText};
+  }
+
+  transition: 0.2s;
+`;
