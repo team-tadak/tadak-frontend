05377d5bde6d0bf64233cc9efeb883ef403c2c24
HaJeong <doolim98@gmail.com>
2022-11-12 10:47:15 +0900

    feat: add onChagne, onError props

--- src/components/common/Inputs/InputUserName/index.js
+++ src/components/common/Inputs/InputUserName/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { ErrorMessageBox, InputBox, InputContainer, LabelBox } from  "components/common/Inputs/styles";
 
-function InputUserName() {
+function InputUserName({onChange,onError}) {
 
     let errorMessage = "";
 
@@ -20,7 +20,7 @@ function InputUserName() {
         else {
             errorMessage = "닉네임(한글 또는 영문 최대 10자)을 다시 입력하세요.";
             errorMessageBox.innerHTML = errorMessage;
-            console.log(errorMessage);
+            onError();
         }
 
         errorMessageBox.innerHTML = errorMessage;
@@ -29,7 +29,7 @@ function InputUserName() {
     return(
         <InputContainer>
             <LabelBox htmlFor="username">닉네임</LabelBox>
-            <InputBox type="text" name="username" placeholder="닉네임을 입력하세요." onBlur={handleBlur}></InputBox>
+            <InputBox type="text" name="username" placeholder="닉네임을 입력하세요." onBlur={handleBlur} onChange={onChange}></InputBox>
             <ErrorMessageBox id="userNameErrorMessageBox"></ErrorMessageBox>
         </InputContainer>
        
