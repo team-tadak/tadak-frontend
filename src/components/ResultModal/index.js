89b8007af7abba3e9ab61c64471db1a8ee4582de
jaychang99 <jaychang99@gmail.com>
2022-11-13 10:09:51 +0900

    feat: ResultModal 에 createPortal 추가

--- src/components/ResultModal/index.js
+++ src/components/ResultModal/index.js
@@ -1,26 +1,36 @@
-import TimerIcon from "assets/svgs/Tumer_light.svg"
-import {
-    ConfirmButton,
-    Modal,
-    NormalText,
-    SpeedText,
-    Icon
-} from "components/ResultModal/style"
-
+import TimerIcon from "assets/svgs/Tumer_light.svg";
+import { ConfirmButton, Modal, NormalText, SpeedText, Icon } from "components/ResultModal/style";
+import { createPortal } from "react-dom";
+import { useState, useEffect } from "react";
 
 // ex) <Result speed="113" onClick={() => console.log("hey")} />
 function ResultModal(props) {
-    return (
-        <Modal>
-            <Icon src={TimerIcon} />
-            <NormalText> 내 타자 기록은 </NormalText>
-            <SpeedText> {props.speed} 타</SpeedText>
-            <ConfirmButton onClick={props.onClick}>
-                <NormalText>확인</NormalText>
-            </ConfirmButton>
-        </Modal>
-    )
-}
+  const [mounted, setMounted] = useState(false);
 
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const modal = (
+    <>
+      {props.open && (
+        <Modal>
+          <Icon src={TimerIcon} />
+          <NormalText> 내 타자 기록은 </NormalText>
+          <SpeedText> {props.speed} 타</SpeedText>
+          <ConfirmButton onClick={props.onClick}>
+            <NormalText>확인</NormalText>
+          </ConfirmButton>
+        </Modal>
+      )}
+    </>
+  );
+
+  if (mounted) {
+    return createPortal(modal, document.getElementById("portal"));
+  } else {
+    return null;
+  }
+}
 
 export default ResultModal;
