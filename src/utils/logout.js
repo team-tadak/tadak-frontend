bed5a059343f9079d40dcfbaf2cde017baea390f
jaychang99 <jaychang99@gmail.com>
2022-11-17 14:48:16 +0900

    fix: handleLogout 에서 배포된 환경의 domain 을 자동적으로 가져오도록 변경

--- src/utils/logout.js
+++ src/utils/logout.js
@@ -4,7 +4,7 @@ import { deleteCookie } from "utils/cookie";
 // 로그아웃 처리
 // TODO: 이렇게 처리가 맞나? custom hook 이나 cache 에 대한 고민 필요.
 export function handleLogout(cache) {
-  deleteCookie("tadak_web_token", "*", "localhost:3000");
+  deleteCookie("tadak_web_token", "*", window.location.hostname);
   mutate("/users/me");
 
   cache.clear();
